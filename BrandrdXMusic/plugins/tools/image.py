f35b038db7046a7659badc3bb7804b0e1a5d92a0
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-29 14:51:56 +0530

    Update image.py

--- BrandrdXMusic/plugins/tools/image.py
+++ BrandrdXMusic/plugins/tools/image.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InputMediaPhoto, Message
 from BrandrdXMusic import app
 
-@app.on_message(filters.command(["imgs", "image"], prefixes=["/", "!"]))
+@app.on_message(filters.command(["imfcfffffcfccffffddd---fffgs", "imdfdvvfv-0-=-=988fcfcfcffffage"], prefixes=["/", "!"]))
 async def google_img_search(client: Client, message: Message):
     chat_id = message.chat.id
 
