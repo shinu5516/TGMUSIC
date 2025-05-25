6b6b4ea3cecd97c88d7628ba0ac7002ad4c49658
shinu5516 <tgmusic5516@gmail.com>
2025-05-25 18:42:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "5883055212"))
+OWNER_ID = int(getenv("OWNER_ID", "8115053977"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -35,15 +35,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bolt9923/musicbot",
+    "https://github.com/shinu5516/TGMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/allinoneearning101")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/thelegendboyss")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/best_hindi_shayrii")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+sKb208JObOExNTJl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
